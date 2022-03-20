5b9b4ad3d6b0c1c85357c181c7ab5d40df574983
chrisrocco <cjrocco@quinnipiac.edu>
2022-03-19 23:17:51 -0400

    Workout Fragment LandScape

--- app/src/main/java/edu/quinnipiac/ser210/workout/WorkoutDetailFragment.java
+++ app/src/main/java/edu/quinnipiac/ser210/workout/WorkoutDetailFragment.java
@@ -9,50 +9,21 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link WorkoutDetailFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class WorkoutDetailFragment extends Fragment {
 
    private long workoutId;
 
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public WorkoutDetailFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment WorkoutDetailFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static WorkoutDetailFragment newInstance(String param1, String param2) {
-        WorkoutDetailFragment fragment = new WorkoutDetailFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+        if (savedInstanceState != null) {
+           workoutId = savedInstanceState.getLong("workoutId");
         }
     }
 
@@ -61,6 +32,10 @@ public class WorkoutDetailFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_workout_detail, container, false);
+    }
+
+    public void onSavedInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putLong("workoutId",workoutId);
     }
     public void setWorkout(long id) {
         this.workoutId = id;
